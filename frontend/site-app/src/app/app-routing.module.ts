9527ba27b6a2440ea00b6246210ca22644defe76
YStoian Ioana Paula <ioana.paula97@yahoo.com>
2022-04-25 20:48:00 +0300

    Add frontend home-page and page-not-found

--- frontend/site-app/src/app/app-routing.module.ts
+++ frontend/site-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {NotFoundComponent} from "./components/pages/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent }
+]; // sets up routes constant where you define your routes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/site-app/src/app/app.module.ts
+++ frontend/site-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
