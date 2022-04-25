925224eac7457613e30669754cbbd13924376906
YStoian Ioana Paula <ioana.paula97@yahoo.com>
2022-04-25 20:00:18 +0300

    "backend and frontend initial setup"

---
+++ backend/src/test/java/com/bdsa/disertatie/backend/backend/MyMoviesListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdsa.disertatie.backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
