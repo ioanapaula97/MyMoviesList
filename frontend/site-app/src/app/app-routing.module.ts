46ef40947d15b3bcfb6b729730f23e74baba9559
YStoian Ioana Paula <ioana.paula97@yahoo.com>
2022-06-02 18:27:38 +0300

    search page

--- frontend/site-app/src/app/app-routing.module.ts
+++ frontend/site-app/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/pages/home/home.component";
 import {NotFoundComponent} from "./components/pages/not-found/not-found.component";
 import {VideoDetailsComponent} from "./components/system/video-details/video-details.component";
+import {VideoFiltersComponent} from "./components/system/video-filters/video-filters.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'detalii-film/:id', component: VideoDetailsComponent },
+  { path: 'search', component: VideoFiltersComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: NotFoundComponent }
 ]; // sets up routes constant where you define your routes
